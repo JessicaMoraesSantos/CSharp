80b5ac17f54096679dfbe9124f28c1bdadfdd112
Jessica Santos <jm.jeska@gmail.com>
2022-03-03 11:16:50 -0300

    Tipos de referência e tipos de valor

---
+++ POO/ByteBank/02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_ByteBank
+{
+    internal class ContaCorrente
+    {
+        public string titular;
+        public int NumeroAgencia;
+        public int NumeroConta;
+        public double saldo = 1000;
+    }
+}
---
+++ POO/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TIPOS DE REFERÊNCIA E TIPOS DE VALOR
+            ContaCorrente conta = new ContaCorrente();
+
+            Console.WriteLine("Saldo: " + conta.saldo);
+            Console.WriteLine("Conta: " + conta.NumeroConta); // Imprime valor default da classe = 0
+            Console.ReadLine();
+        }
+    }
+}
