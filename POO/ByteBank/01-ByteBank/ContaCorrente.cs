633893bbf827ed194e1244b940188421e63763c7
Jessica Santos <jm.jeska@gmail.com>
2022-03-03 11:14:15 -0300

    Criando a primeira classe

---
+++ POO/ByteBank/01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_ByteBank
+{
+    internal class ContaCorrente
+    {
+        public string titular;
+        public int NumeroAgencia;
+        public int NumeroConta;
+        public double saldo;
+    }
+}
---
+++ POO/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(); //new = criar objeto
+
+            conta.titular = "Luan";
+            conta.NumeroAgencia = 0001;
+            conta.NumeroConta = 123456;
+            conta.saldo = 5151;
+
+            Console.WriteLine("Titular: " + conta.titular);
+            Console.WriteLine("Agencia: " + conta.NumeroAgencia);
+            Console.WriteLine("Conta: " + conta.NumeroConta);
+            Console.WriteLine("Saldo: " + conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
