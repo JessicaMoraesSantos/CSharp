5b103b2d5f98b852bd3471ec72d4f6bbd6c574ad
Jessica Santos <jm.jeska@gmail.com>
2022-03-03 11:24:54 -0300

    Tipos de referência e tipos de valor

---
+++ POO/ByteBank/03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_ByteBank
+{
+    internal class ContaCorrente
+    {
+        public string titular;
+        public int NumeroAgencia;
+        public int NumeroConta;
+        public double saldo = 1000;
+
+        public bool Sacar (double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+    }
+}
---
+++ POO/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.NumeroConta = 386;
+            contaDaGabriela.NumeroAgencia = 123456;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.NumeroConta = 386;
+            contaDaGabrielaCosta.NumeroAgencia = 123456;
+
+            //Tipos de referência e tipos de valor
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaCosta)); // Retorna falso porque cada nova instância referencia um endereço diferente na memória
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez)); // Retorna true
+
+            contaDaGabriela = contaDaGabrielaCosta;
+
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta); // Retorne true porque é a mesma referência
+
+            Console.ReadLine();
+
+        }
+    }
+}
